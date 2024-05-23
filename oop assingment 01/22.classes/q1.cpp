14867aea1faad30440232856440ab0c01d98ca5c
Marzia Mahabuba Chowdhury Momo <margiachowdhury@gmail.com>
2024-05-23 22:37:52 +0600

    Add files via upload

---
+++ oop assingment 01/22.classes/q1.cpp
@@ -0,0 +1,31 @@
+//Create a class `Circle` with a private radius and public functions to set the radius and calculate the area of the circle.
+
+
+#include<bits/stdc++.h>
+using namespace std;
+
+class Circle
+{
+    private:
+        float radius;
+    public:
+        void setRadius(float r)
+        {
+            radius = r;
+        }
+        float getArea()
+        {
+            return 3.14 * radius * radius;
+        }
+};
+
+int main()
+{
+    Circle c;
+    float r;
+    cout << "Enter radius: ";
+    cin >> r;
+    c.setRadius(r);
+    cout << "Area of circle with radius " << r << " = " << c.getArea() << endl;
+    return 0;
+}
